a36a74df2dd6fe1874dd13c62b8ad8070c8ba5f9
Andttrea <andreavg@lcg.unam.mx>
2025-04-03 16:04:50 -0600

    Se agregó un programa para eliminar los primeros 14 adaptadores de una secuencia, el cual creo un archivo de salida con los resultados.

--- bin/3.adap_seq.py
+++ bin/3.adap_seq.py
@@ -1,1 +1,8 @@
-# 
+# Este programa se trata de eliminar los primeros 14 adaptadores de una secuencia dada.
+
+with open('data/4_input_adapters.txt', 'r')as infile, open('results/4_output_adapters.txt', 'w') as outfile:
+    for line in infile:
+        no_primers = line.strip()[14:]
+
+        outfile.write(f'{no_primers}\n')
+    

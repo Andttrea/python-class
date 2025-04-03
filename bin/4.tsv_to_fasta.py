b934ea781f8d62c6df904bbecd905ad41abb84e9
Andttrea <andreavg@lcg.unam.mx>
2025-04-03 16:18:55 -0600

    Se agregó un programa para convertir secuencias en formato TSV a formato FASTA, junto con un archivo de entrada de secuencias y un archivo de salida con los resultados.

---
+++ bin/4.tsv_to_fasta.py
@@ -0,0 +1,8 @@
+# Este es un programa que convierte las secuencias en fomrato tsv a formato fasta.
+
+with open('data/dna_sequences.txt', 'r') as infile, open('results/dna_sequences.fa', 'w') as outfile:
+    for line in infile:
+        columns = line.strip().split('\t') #strip elimina los espacios y split separa la cadena ṕor tabuladores, cada uno de esos les asigna indices
+
+        outfile.write  (f'>{columns[0]}\n{columns[1].upper()}\n') # upper convierte la cadena a mayúsculas
+    

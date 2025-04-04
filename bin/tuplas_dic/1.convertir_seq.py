48e46eb246b1ef8e99ab993ad5e242fadd14ae67
Andttrea <andreavg@lcg.unam.mx>
2025-04-04 12:56:49 -0600

    Se eliminaron programas obsoletos para el manejo de secuencias de ADN y se añadieron nuevas implementaciones en listas.

--- bin/tuplas_dic/1.convertir_seq.py
+++ bin/tuplas_dic/1.convertir_seq.py
@@ -1,5 +1,5 @@
 #este ejercicio va a convertir una secuencia a una tupla
 
 secuencia = tuple("ATGCGTAGC")
-for base in secuencia:
-    print(base, end="")
+    for base in secuencia:
+        print(base, end="")
---
+++ bin/tuplas_dic/2.cont_freq.py
@@ -0,0 +1,5 @@
+# etse programa cuantas veces aparece cada base en una tupla
+
+secuencia = tuple("ATGCGTAGC")
+for base in secuencia:
+    print("tupla".count(base), end="")

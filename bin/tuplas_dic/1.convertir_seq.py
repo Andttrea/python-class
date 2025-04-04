49d6ced175f1afdfd6e043eeee4ff1c77b1c64de
Andttrea <andreavg@lcg.unam.mx>
2025-04-04 13:05:04 -0600

    Se corrigió la conversión de secuencia a tupla y se mejoró el conteo de nucleótidos con diferentes métodos.

--- bin/tuplas_dic/1.convertir_seq.py
+++ bin/tuplas_dic/1.convertir_seq.py
@@ -1,5 +1,6 @@
 #este ejercicio va a convertir una secuencia a una tupla
 
-secuencia = tuple("ATGCGTAGC")
-    for base in secuencia:
-        print(base, end="")
+secuencia = "ATGCGTAGC"
+
+for base in secuencia:
+    print(base)
--- bin/tuplas_dic/2.cont_freq.py
+++ bin/tuplas_dic/2.cont_freq.py
@@ -1,5 +1,18 @@
 # etse programa cuantas veces aparece cada base en una tupla
 
 secuencia = tuple("ATGCGTAGC")
-for base in secuencia:
-    print("tupla".count(base), end="")
+
+print(secuencia.count("A"))
+print(secuencia.count("T"))
+print(secuencia.count("G"))
+print(secuencia.count("C"))
+
+#Forma 2 = comprensión de listas
+bases = list("ATGC")
+freq = {base: secuencia.count(base) for base in bases}
+print(freq) 
+
+
+#Forma 3 ciclos
+for base in "ACGT":
+    print(f"{secuencia.count(base)} bases {base}")
